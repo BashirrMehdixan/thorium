0a8797bd1604265c1b88efdf0157abc7afb95aa7
Bashirr <beshirr096@gmail.com>
2024-07-11 15:55:30 +0400

    navbar, services section and contact-links icons changed

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,17 @@
 const searchBox = document.querySelector('.search');
 const searchBtn = document.querySelector('.search-btn');
 const searchInp = document.querySelector('.desk-input');
+const currentLang = document.querySelector('.current');
+const langs = document.querySelector('.languages');
+const icon = document.querySelector('.language .icon');
+const openNav = document.querySelector('.open-nav');
+const closeNav = document.querySelector('.close-nav');
+const navBar = document.querySelector('nav');
+const sectionNav = document.querySelector('.section-nav');
+const upIcon = document.querySelector('.up-icon');
+const stickyIcons = document.querySelector('.contact-links');
+const footer = document.querySelector('footer');
+const servicesSection = document.querySelector('.services-section');
 
 searchBtn.addEventListener('click', (e) => {
     e.preventDefault();
@@ -13,21 +24,12 @@ document.addEventListener('click', e => {
         searchBox.classList.remove('active');
         searchInp.classList.remove('active');
     }
-})
-
-const currentLang = document.querySelector('.current');
-const langs = document.querySelector('.languages');
-const icon = document.querySelector('.language .icon');
+});
 
 currentLang.addEventListener('click', (e) => {
     langs.classList.toggle('active');
     icon.classList.toggle('active');
-})
-
-const openNav = document.querySelector('.open-nav');
-const closeNav = document.querySelector('.close-nav');
-const navBar = document.querySelector('nav');
-const sectionNav = document.querySelector('.section-nav');
+});
 
 openNav.addEventListener('click', (e) => {
     navBar.classList.add('active');
@@ -41,59 +43,34 @@ closeNav.addEventListener('click', (e) => {
     openNav.style.display = 'block';
 });
 
-const upIcon = document.querySelector('.up-icon');
-const stickyIcons = document.querySelector('.contact-links');
 window.onscroll = function () {
-    if (sectionNav) {
-        if (window.scrollY > 50) {
-            sectionNav.style.backgroundColor = "#fff";
-        } else {
-            sectionNav.style.backgroundColor = "transparent";
-        }
-    }
-
     if (window.scrollY > 300) {
         upIcon.classList.add('show');
         stickyIcons.classList.add('active');
     } else {
-
-
-    }
-
-    if (window.scrollY != 0) {
-        if (window.scrollY > 5264) {
-            stickyIcons.classList.remove('active');
-
-        } else {
-            stickyIcons.classList.add('active');
-        }
-    } else {
+        upIcon.classList.remove('show');
         stickyIcons.classList.remove('active');
     }
 };
 
-
-
-const videos = document.querySelectorAll('video');
-videos.forEach(video => {
-    video.addEventListener('mouseenter', function () {
-        videos.forEach(v => {
-            if (v !== video) {
-                v.pause();
-                v.nextElementSibling.style.display = 'block';
-            }
-        });
-        video.play();
-    });
-
-    video.addEventListener('mouseleave', function () {
-        videos.forEach(v => {
-            v.nextElementSibling.style.display = 'none';
-            v.play();
-        });
-    });
-});
-
 Fancybox.bind("[data-fancybox]");
+
+const options = {
+    root: null,
+    rootMargin: '0px',
+    threshold: .5
+};
+
+const observerFooter = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            stickyIcons.style.display = 'none';
+        } else {
+            stickyIcons.style.display = 'block';
+        }
+    });
+}, options);
+
+observerFooter.observe(footer);
 
 AOS.init();
